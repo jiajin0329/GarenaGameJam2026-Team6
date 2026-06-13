49f4cd5cc404b17b14eeb7e3f71fdac9b5767f8c
Jill505 <orangec.org@gmail.com>
2026-06-13 19:37:38 +0800

    feat - StorySceneUpload

--- Assets/Ankhra/StartSceneManager.cs
+++ Assets/Ankhra/StartSceneManager.cs
@@ -1,6 +1,89 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using System.Collections;
 public class StartSceneManager : MonoBehaviour
 {
+    public GameObject[] StoryGameObject = new GameObject[18];
+    public ScrollRect myScrollRect;
 
+    public int totalStoryElementIndex = 18;
+
+    public int currentIndex = 0;
+
+    [SerializeField] private ScrollRect scrollRect;
+    [SerializeField] private float duration = 0.25f;
+
+
+    public void Start()
+    {
+        InitFunc();
+    }
+
+    public void Update()
+    {
+        Debug.Log("Waht wver");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NextPageEvent();
+        }
+    }
+
+    public void InitFunc()
+    {
+        totalStoryElementIndex = StoryGameObject.Length;
+    }
+
+    public void NextPageEvent()
+    {
+        currentIndex += 1;
+        if (currentIndex < totalStoryElementIndex)
+        {
+            //Load next page
+            StoryGameObject[currentIndex].SetActive(true);
+            ScrollToBottomSmooth();
+        }
+    }
+
+
+
+    private Coroutine scrollCoroutine;
+
+    public void ScrollToBottomSmooth()
+    {
+        if (scrollCoroutine != null)
+            StopCoroutine(scrollCoroutine);
+
+        scrollCoroutine = StartCoroutine(ScrollToBottomCoroutine());
+    }
+
+    private IEnumerator ScrollToBottomCoroutine()
+    {
+        // 等 UI Layout 更新
+        yield return null;
+
+        Canvas.ForceUpdateCanvases();
+        LayoutRebuilder.ForceRebuildLayoutImmediate(scrollRect.content);
+
+        float start = scrollRect.verticalNormalizedPosition;
+        float end = 0f;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.unscaledDeltaTime;
+
+            float t = Mathf.Clamp01(elapsed / duration);
+
+            // SmoothStep 讓移動比較柔和
+            t = Mathf.SmoothStep(0f, 1f, t);
+
+            scrollRect.verticalNormalizedPosition = Mathf.Lerp(start, end, t);
+
+            yield return null;
+        }
+
+        scrollRect.verticalNormalizedPosition = end;
+        scrollCoroutine = null;
+    }
 }
+
